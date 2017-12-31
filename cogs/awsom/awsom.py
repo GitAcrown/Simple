7dd6d348d1d0ccdfe532b04c43abaedc95346612
GitAcrown <Wellington>
2017-12-31 17:54:44 +0100

    Awsom | MSGJDL

--- cogs/awsom/awsom.py
+++ cogs/awsom/awsom.py
@@ -24,15 +24,15 @@ class Awsom:
         try:
             await asyncio.sleep(5)  # Temps de mise en route
             do = True
-            channel = self.bot.get_channel("329071582129422337")
+            channel = self.bot.get_channel("204585334925819904")
             while do:
                 date = time.strftime("%d/%m/%Y", time.localtime())
                 heure = time.strftime("%H:%M", time.localtime())
-                if date == "31/12/2017" and heure == "17:53":
+                if date == "01/01/2018" and heure == "00:00":
                     await self.bot.send_message(channel, "Chers EKheysiens, je vous souhaite une excellente année 2018 "
                                                          "(meilleure que 2017 qui était pas ouf), "
                                                          "j'espère que vous réussirez tous à faire ce que vous désirez "
-                                                         "et j'espère passer encore d'extraordinaires moments avec vous"
+                                                         "et j'espère passer encore d'extraordinaires moments avec vous."
                                                          "\n**Bonne année à tous !**\n\n- Acrown")
                     do = False
                 await asyncio.sleep(10)
