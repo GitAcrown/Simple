93d6c3105d20f805c84708fd4cfa94f59180535e
GitAcrown <Wellington>
2018-01-08 21:23:19 +0100

    PRISM | JENPEUPLUDECEBUGDISCORD

--- cogs/prism/prism.py
+++ cogs/prism/prism.py
@@ -610,7 +610,8 @@ class Prism:  # MODULE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
                             msgtxt += "**{}** {}\n".format(nom, num)
                             total += num
                         botmsg = self.day_stats_num(server, date, "BOT_MSG")
-                        msgtxt += "\n**Total** {}\n**Bots exclus** {}".format(total, total - botmsg)
+                        msgtxt += "**Total** {}\n" \
+                                  "**Bots exclus** {}".format(total, (total - botmsg))
                         em.add_field(name="Messages", value=msgtxt)
 
                     data = {}
