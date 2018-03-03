698256b6fd1804a6be370ebee413d964c081ffd5
GitAcrown <Wellington>
2018-03-03 02:22:50 +0100

    Agora | Ajout du référendum (+ maj msg social)

--- cogs/agora/agora.py
+++ cogs/agora/agora.py
@@ -488,9 +488,10 @@ class Agora:
                 if qr[1].upper() in self.sys["POLLS"]:
                     pid = qr[1].upper()
                     poll = self.sys["POLLS"][pid]
-                    if poll["AUTEUR"] != ctx.message.author.id or not ctx.message.author.server_permissions.ban_members:
-                        await self.bot.say("**Erreur** | Vous n'êtes pas le propriétaire du sondage")
-                        return
+                    if poll["AUTEUR"] != ctx.message.author.id:
+                        if not ctx.message.author.server_permissions.ban_members:
+                            await self.bot.say("**Erreur** | Vous n'êtes pas le propriétaire du sondage")
+                            return
                     mess = await self.bot.get_message(ctx.message.channel, poll["MSGID"])
                     if mess:
                         await self.bot.unpin_message(mess)
